265202f6bccd7c8e4d386c05fb725955c13f38cf
Marco Baxemyr <baxemyr@gmail.com>
2019-08-30 14:48:00 +0200

    Configured dev and prod envs

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-x31wwa76o64y"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://serverless-stack.phelerox.eu/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    USER_POOL_ID: "us-east-1_viE32FXKe",
+    APP_CLIENT_ID: "4l5qua34n7uis4quusphhoboo0",
+    IDENTITY_POOL_ID: "us-east-1:43026d40-b74b-4d61-bca7-9856ac406a74"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-ink3zcamuhvl"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://serverless-stack.phelerox.eu/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_EVmh8cz9q",
+    APP_CLIENT_ID: "6tt138lm1e1cj49hqrr55dl3u6",
+    IDENTITY_POOL_ID: "us-east-1:45960fa9-1b26-49b3-872c-711b415249d8"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
