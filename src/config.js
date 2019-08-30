88e0ee288fbd0e1cea7f5af7fb8e1352dded2f67
Marco Baxemyr <baxemyr@gmail.com>
2019-08-30 14:58:40 +0200

    Adding stripe test key to config.js. Bad idea, but sure!

--- src/config.js
+++ src/config.js
@@ -12,7 +12,8 @@ const dev = {
     USER_POOL_ID: "us-east-1_viE32FXKe",
     APP_CLIENT_ID: "4l5qua34n7uis4quusphhoboo0",
     IDENTITY_POOL_ID: "us-east-1:43026d40-b74b-4d61-bca7-9856ac406a74"
-  }
+  },
+  STRIPE_KEY: "sk_test_VUUVIOLGDJ1y5LWQCt9S7osB00ulOv5PfS",
 };
 
 const prod = {
@@ -29,7 +30,8 @@ const prod = {
     USER_POOL_ID: "us-east-1_EVmh8cz9q",
     APP_CLIENT_ID: "6tt138lm1e1cj49hqrr55dl3u6",
     IDENTITY_POOL_ID: "us-east-1:45960fa9-1b26-49b3-872c-711b415249d8"
-  }
+  },
+  STRIPE_KEY: "sk_test_VUUVIOLGDJ1y5LWQCt9S7osB00ulOv5PfS",
 };
 
 // Default to dev if not set
